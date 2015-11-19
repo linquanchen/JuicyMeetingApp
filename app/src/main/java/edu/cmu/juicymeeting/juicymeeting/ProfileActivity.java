cadfe80292aabc376874c6102061bc4783d88503
Jeffery <lqchen613@gmail.com>
2015-11-18 19:27:40 -0800

    Change the profile layout

--- app/src/main/java/edu/cmu/juicymeeting/juicymeeting/ProfileActivity.java
+++ app/src/main/java/edu/cmu/juicymeeting/juicymeeting/ProfileActivity.java
@@ -2,9 +2,7 @@ package edu.cmu.juicymeeting.juicymeeting;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
