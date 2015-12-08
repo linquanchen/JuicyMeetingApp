1bfad1da752e73ee88ca62755a5779810c7fee18
Jeffery <lqchen613@gmail.com>
2015-12-07 18:35:36 -0800

    Connect to server

---
+++ app/src/main/java/edu/cmu/juicymeeting/util/RESTfulAPI.java
@@ -0,0 +1,10 @@
+package edu.cmu.juicymeeting.util;
+
+/**
+ * Created by chenlinquan on 12/7/15.
+ */
+public class RESTfulAPI {
+    private static String DNS = "http://ec2-52-91-106-6.compute-1.amazonaws.com/";
+
+    public static String upcomingEventURL = DNS + "webapi/event/upcoming/";
+}
